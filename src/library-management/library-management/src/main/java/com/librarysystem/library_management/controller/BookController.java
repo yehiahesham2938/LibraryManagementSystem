9fce81fe3de2b4b82c85f785e4b0521ce0193a70
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-12-16 19:36:50 +0200

    Finished the view books page and linked it inside BookController to view all books in DB, Looking for how to display cover photos in the table

--- src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/BookController.java
+++ src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
@@ -30,6 +31,9 @@ public class BookController {
         model.addAttribute("book", new Book());
         return "Admin/addBook";
     }
+
+
+
 
     @GetMapping("/view-books")
     public String viewBooks(Model model) {
