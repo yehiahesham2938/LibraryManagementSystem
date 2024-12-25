4c3ad18984933e4837a7ce04bfc8c7ff03cb399e
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-12-25 21:06:01 +0200

    LibraryManagementSystem LoginPage, DB Integration

---
+++ src/library-management/library-management/src/main/java/com/librarysystem/library_management/LibraryManagementApplication.java
@@ -0,0 +1,16 @@
+package com.librarysystem.library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+
+}
