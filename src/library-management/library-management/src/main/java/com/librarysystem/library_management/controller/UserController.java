1c546707ebde647c1dacc2dece091ff243e546d2
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-12-28 21:01:54 +0200

    Created APIs for User Navbar

--- src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/UserController.java
+++ src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/UserController.java
@@ -17,17 +17,35 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private BookRepository bookRepository;
 
     @GetMapping("")
-    public String showUserPage(){
+    public String showUserPage()
+    {
+        return "User/home";
+
+    }
+
+    @GetMapping("/home")
+    public String ViewBooks(){
         return "User/home";
     }
 
-//    @GetMapping("/home")
-//    public String ViewBooks
+    @GetMapping("/ViewProfile")
+    public String ViewProfile(){
+        return "User/ViewProfile";
+    }
+
+    @GetMapping("/About")
+    public String About(){
+        return "User/About";
+    }
+    @GetMapping("/ContactUs")
+    public String ContactUs(){
+        return "User/ContactUs";
+    }
 
 }
