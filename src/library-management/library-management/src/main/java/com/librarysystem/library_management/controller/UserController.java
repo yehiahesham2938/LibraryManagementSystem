ddfa9c74c4183146c4b1139935575333085c788b
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-12-28 22:48:36 +0200

    Created homepage with borrow button for user page

--- src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/UserController.java
+++ src/library-management/library-management/src/main/java/com/librarysystem/library_management/controller/UserController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Base64;
+import java.util.List;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -29,10 +32,10 @@ public class UserController {
 
     }
 
-    @GetMapping("/home")
-    public String ViewBooks(){
-        return "User/home";
-    }
+//    @GetMapping("/home")
+//    public String ViewBooks(){
+//        return "User/home";
+//    }
 
     @GetMapping("/ViewProfile")
     public String ViewProfile(){
@@ -48,4 +51,24 @@ public class UserController {
         return "User/ContactUs";
     }
 
+    @GetMapping("/home")
+    public String viewBooks(Model m) {
+        List<Book> books = bookRepository.findAll();
+
+        for (Book book : books) {
+            if (book.getCoverPhoto() != null) {
+                String encodedImage = Base64.getEncoder().encodeToString(book.getCoverPhoto());
+                book.setEncodedCoverPhoto(encodedImage);
+            }
+        }
+
+        m.addAttribute("books", books);
+        return "User/home";
+    }
+
+
+
 }
+
+
+
