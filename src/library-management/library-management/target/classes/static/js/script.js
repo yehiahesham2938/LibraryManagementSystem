fa8b01b67afdec6e3317ba3ac8c255e462d722b7
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2024-12-30 22:13:18 +0200

    LastCommit

---
+++ src/library-management/library-management/target/classes/static/js/script.js
@@ -0,0 +1,8 @@
+const websitesDesigned = document.querySelector(".websites-designed");
+const appsdeveloped = document.querySelector(".apps-developed");
+
+setTimeout(() => {
+    websitesDesigned.innerHTML = "76";
+    appsdeveloped.innerHTML = "32";
+}, 400);
+
